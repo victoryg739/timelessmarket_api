7b0599013ff05651f9b119499df00d9e70042454
victoryg739 <56720210+victoryg739@users.noreply.github.com>
2020-10-08 23:23:42 +0800

    Update yahoofin.py

--- yahoofin.py
+++ yahoofin.py
@@ -1,10 +1,12 @@
 import yfinance as yf
+import json
 
 def get_info(ticker):
 
     stock = yf.Ticker(ticker)
 
     # get stock info
+    
+    return json.dumps(stock.info)
 
-    return stock.info
 

b7ac54189cad568b1ea6eef4c5215f5d0df30073
victoryg739 <56720210+victoryg739@users.noreply.github.com>
2021-01-18 03:08:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ from scrap import scrapData
 from yahoofin import scrap_yahoofin
 from flask_cors import CORS
 from get_ticker import ticker
+from selenium import webdriver
+
 app = Flask(__name__)
 CORS(app)
 
 #Later change to production deployment
+
+@app.route('/<ticker>')
+def tested(ticker):
+    ticker = "aapl"
+    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    url = "https://money.tmx.com/en/quote/" + ticker + ":US/financials-filings"
+    driver.get(url)
+    html = driver.page_source
+    return(html)
 
 @app.route('/')
 def hello_world():
